1ca9decfc07d1fb761add2548b8178daff616224
TheFifthRider <flare777@gmail.com>
2020-03-05 18:05:43 -0500

    fix: correctly implemented Nav and markdown parsing

--- simplyroleplaying/setup.py
+++ simplyroleplaying/setup.py
@@ -37,7 +37,7 @@ nav.register_element('navbar', Navbar(
              ),
     View('Magic', '.magic'),
     Subgroup('World',
-             View('Overview', '.world'),
+             View('Overview', '.world_overview'),
              View('Races', '.world_races'),
              View('Religions', '.world_religions'),
              ),
@@ -66,99 +66,97 @@ def index():
     return render_template('index.html', headerText=header_text, pageTitle=page_title)
 
 
-@srp.route("/character/")
+@srp.route("/character")
 def character_creation():
     return render_markdown('character/creation.md')
 
 
-@srp.route("/character/attributes/")
-def stats():
+@srp.route("/character/attributes")
+def attributes():
     return render_markdown('character/attributes.md')
 
 
-@srp.route("/character/skills/")
+@srp.route("/character/skills")
 def skills():
     return render_markdown('character/skills.md')
 
 
-@srp.route("/character/tests/")
+@srp.route("/character/tests")
 def tests():
     return render_markdown('character/tests.md')
 
 
-@srp.route("/character/milestones/")
+@srp.route("/character/milestones")
 def milestones():
     return render_markdown('character/milestones.md')
 
 
-@srp.route("/character/advantages_and_disadvantages/")
+@srp.route("/character/advantages_and_disadvantages")
 def advantages_and_disadvantages():
     return render_markdown('character/advantages_and_disadvantages.md')
 
 
-@srp.route("/action/")
+@srp.route("/action")
 def action():
     return render_markdown('action/action.md')
 
 
-@srp.route("/action/combat/")
+@srp.route("/action/combat")
 def combat():
     return render_markdown('action/combat.md')
 
 
-@srp.route("/action/racing/")
+@srp.route("/action/racing")
 def racing():
     header_text = "Racing"
     page_title = "Racing"
     return render_template('combat_and_equipment/racing.html', headerText=header_text, pageTitle=page_title)
 
 
-@srp.route("/action/weapons/")
+@srp.route("/action/weapons")
 def weapons():
-    header_text = "Weapons"
-    page_title = "Weapons"
-    return render_template('combat_and_equipment/weapons.html', headerText=header_text, pageTitle=page_title)
+    return render_markdown('action/weapons.md')
 
 
-@srp.route("/action/techniques/")
+@srp.route("/action/techniques")
 def techniques():
     header_text = "Combat Techniques"
     page_title = "Combat Techniques"
     return render_template('combat_and_equipment/techniques.html', headerText=header_text, pageTitle=page_title)
 
 
-@srp.route("/action/armor/")
+@srp.route("/action/armor")
 def armor():
     return render_markdown('action/armor.md')
 
 
-@srp.route("/combat/healing/")
+@srp.route("/combat/healing")
 def healing():
     header_text = "Healing"
     page_title = "Healing"
     return render_template('combat_and_equipment/healing.html', headerText=header_text, pageTitle=page_title)
 
 
-@srp.route("/magic/")
+@srp.route("/magic")
 def magic():
     header_text = "Simply Magic"
     page_title = "Simply Magic Module"
     return render_template('magic.html', headerText=header_text, pageTitle=page_title)
 
 
-@srp.route("/world/")
+@srp.route("/world")
 def world_overview():
     return render_markdown('worlds/earth_the_forgotten_times/overview.md')
 
 
-@srp.route("/world/races/")
+@srp.route("/world/races")
 def world_races():
     header_text = "The Races of Earth: The Forgotten Times"
     page_title = "Races - Earth: The Forgotten Times"
     return render_template('earth_the_forgotten_times/races.html', headerText=header_text, pageTitle=page_title)
 
 
-@srp.route("/world/religions/")
+@srp.route("/world/religions")
 def world_religions():
     header_text = "The Religions of Earth: The Forgotten Times"
     page_title = "Religions - Earth: The Forgotten Times"
@@ -166,16 +164,17 @@ def world_religions():
 
 
 def render_markdown(path_to_markdown):
-    page_title = "page"
-    with open(os.path.join(os.path.dirname(__file__), 'markdown/' + path_to_markdown), 'r') as file:
-        file_contents = file.read()
+    resolved_path_to_markdown = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'markdown/' + path_to_markdown)
+    with open(resolved_path_to_markdown, 'r') as file:
         for line in file:
-            trimmed_line = line.strip()
-            if trimmed_line.startswith("#"):
-                page_title = trimmed_line.strip("#").strip()
+            stripped_line = line.strip()
+            if stripped_line.startswith('# '):
+                page_title = stripped_line.strip('# ')
                 break
 
-    return render_template('base.html', pageTitle=page_title, fileContents=file_contents)
+    with open(resolved_path_to_markdown, 'r') as file:
+        file_contents = file.read()
+        return render_template('base.html', pageTitle=page_title, fileContents=file_contents)
 
 
 if __name__ == "__main__":
