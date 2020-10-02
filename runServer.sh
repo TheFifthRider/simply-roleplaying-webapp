b9d90212838fc7110359bd135d8167b639e2bc90
TheFifthRider <flare777@gmail.com>
2020-10-01 22:05:50 -0400

    build: Updating requirements and runServer

--- runServer.sh
+++ runServer.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 if [ "$OSTYPE" = "msys" ]; then
-  echo "Assuming this is windows, running with waitress"
+  echo "Running on windows..."
   source venv/Scripts/activate
-  waitress-serve --port=80 simplyroleplaying.setup:srp
+  cd simplyroleplaying
+  mkdocs serve
 else
-  echo "Assuming this is a linux-based OS, running with gunicorn"
+  echo "Running on linux os..."
   source venv/bin/activate
-  gunicorn -b :80 simplyroleplaying.setup:srp
+  cd simplyroleplaying
+  mkdocs serve
 fi
