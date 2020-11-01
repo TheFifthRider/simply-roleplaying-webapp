06583441058b8ab47661c2e01c1aaf37320eb026
TheFifthRider <flare777@gmail.com>
2020-11-01 17:25:42 -0500

    refactor: Final reorganization to commit to mkdocs

--- runServer.sh
+++ runServer.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
+if [ ! -d "venv" ]; then
+  echo "No virtual environment detected, running first time setup..."
+  virtualenv venv
+  initialize=true
+else
+  initialize=false
+fi
+
 if [ "$OSTYPE" = "msys" ]; then
   echo "Running on windows..."
   source venv/Scripts/activate
-  cd simplyroleplaying
-  mkdocs serve "$@"
 else
   echo "Running on linux os..."
   source venv/bin/activate
-  cd simplyroleplaying
-  mkdocs serve "$@"
 fi
+
+if [ "$initialize" = "true" ]; then
+  pip install -r requirements.txt
+fi
+
+mkdocs serve "$@"
