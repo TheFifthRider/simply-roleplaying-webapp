9f682527ffb5cf137d3435e37322fea4480e1b73
TheFifthRider <flare777@gmail.com>
2020-11-01 19:19:53 -0500

    Deployed 9377c00 with MkDocs version: 1.1.2

---
+++ js/extra.js
@@ -0,0 +1,21 @@
+$( document ).ready($(function( e ){
+  delete Hammer.defaults.cssProps.userSelect;
+
+  var bodyHammer = new Hammer( document.body );
+  var navHammer = new Hammer( document.getElementsByClassName("wy-side-scroll")[0] );
+
+  bodyHammer.on( "swiperight", swipeNavIn );
+  bodyHammer.on( "swipeleft", swipeNavOut );
+  navHammer.on( "swiperight", swipeNavIn );
+  navHammer.on( "swipeleft", swipeNavOut );
+
+  function swipeNavIn ( event ){
+    $("[data-toggle='wy-nav-shift']").addClass("shift");
+    $("[data-toggle='rst-versions']").addClass("shift")
+  }
+
+  function swipeNavOut ( event ){
+    $("[data-toggle='wy-nav-shift']").removeClass("shift");
+    $("[data-toggle='rst-versions']").removeClass("shift")
+  }
+}));
